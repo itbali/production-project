6f5b8256c277851c5a08cb081807539071a35ea3
itbali <alex.itbali@gmail.com>
2023-03-02 23:02:44 +0300

    - added tests for profile page

--- config/.storybook/decorators/storeDecorator.tsx
+++ config/.storybook/decorators/storeDecorator.tsx
@@ -2,9 +2,11 @@ import type { Story } from '@storybook/react';
 import { StateSchema, StoreProvider } from 'app/providers/storeProvider';
 import { DeepPartial, ReducersMapObject } from '@reduxjs/toolkit';
 import { loginReducer } from 'features/AuthByUserName/model/slice/loginSlice';
+import { profileReducer } from 'entities/Profile';
 
 export const defaultAsyncReducers: Partial<ReducersMapObject<StateSchema>> = {
     loginForm: loginReducer,
+    profile: profileReducer,
 }
 
 export const StoreDecorator = (state: DeepPartial<StateSchema>, asyncReducer?:Partial<ReducersMapObject<StateSchema>>)=> (StoryComponent: Story): JSX.Element => (
